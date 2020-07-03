66fae15cd4b93ae3b4b59863f05cec4cc32937e3
kerberos777 <48998348+kerberos777@users.noreply.github.com>
2020-07-03 11:30:59 +0800

    Add files via upload

---
+++ TrojanUser.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+function green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+function red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+function yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+sudo cp server.conf_bk server.conf
+sudo systemctl restart trojan
+
+cat /usr/src/trojan/server.conf
+    red "done!"
